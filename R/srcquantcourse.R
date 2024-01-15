08e79669c7bf4e3fad4cb54a4ab067acd689b1b3
sdaume <stefan.daume@gmail.com>
2024-01-15 11:30:20 +0100

    Add basic R package scaffolding

---
+++ R/srcquantcourse.R
@@ -0,0 +1,9 @@
+#' Resources for the SRC Quantitative Methods PhD Course
+#'
+#' This package collects scripts, references and course notes for the 2024 SRC
+#' Quantitative Methods PhD course.
+#'
+#' @name srcquantcourse
+#' @docType package
+#'
+NULL
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # srcquantcourse
-Resources for the SRC Quantitative Methods PhD course
+
+Resources for the SRC Quantitative Methods PhD course. This repo collects scripts, references and course notes for the course.
