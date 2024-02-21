5d36426268d26b30717caf22ff46ba288c19ea53
sdaume <stefan.daume@gmail.com>
2024-02-21 15:51:13 +0100

    Update package description

--- R/srcquantcourse.R
+++ R/srcquantcourse.R
@@ -1,6 +1,6 @@
-#' Resources for the SRC Quantitative Methods PhD Course
+#' Scripts and exercises for the SRC Quantitative Methods PhD Course
 #'
-#' This package collects scripts, references and course notes for the 2024 SRC
+#' This package collects scripts, data and exercises notes for the 2024 SRC
 #' Quantitative Methods PhD course.
 #'
 #' @name srcquantcourse
